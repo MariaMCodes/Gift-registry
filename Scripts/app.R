21e07ab4338135c39d6866c930d461a27f797520
MMempin421 <maria.mempin@hotmail.com>
2021-02-25 15:35:15 +1100

    Centred image using HTML

--- Scripts/app.R
+++ Scripts/app.R
@@ -3,8 +3,9 @@ library(shiny)
 
 ui <- fluidPage(
   fluidRow(
-    h4("Apple's Birthday Gift Registry", align = "center", style = "color:darkblue"),
-    img(src = "Header.png", align = "center", height = 200, width = 400)
+    h3("Apple's Birthday Gift Registry", align = "center", style = "color:#487AA1"),
+    HTML('<center><img src="Header.png" height="220", width="400"></center>')
+    #img(src = "Header.png", align = "center", height = 200, width = 400)
   )
 )
 
