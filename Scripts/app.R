f70c537c7604eec28c18c94667c2f884e3fb3314
MMempin421 <maria.mempin@hotmail.com>
2021-02-25 15:10:43 +1100

    First commit

---
+++ Scripts/app.R
@@ -0,0 +1,14 @@
+
+library(shiny)
+
+ui <- fluidPage(
+  fluidRow(
+    h4("Apple's Birthday Gift Registry", align = "center", style = "color:darkblue"),
+    img(src = "Header.png", align = "center", height = 200, width = 400)
+  )
+)
+
+server <- function(input, output) {
+  # empty
+}
+shinyApp(ui=ui, server=server)
---
+++ Scripts/ui.R
@@ -0,0 +1,9 @@
+
+library(shiny)
+
+ui <- fluidPage(
+  fluidRow(
+    h4("Apple's Birthday Gift Registry", align = "center", style = "color:darkblue"),
+    img(src = "Header.png", align = "center", height = 200, width = 400)
+  )
+)
