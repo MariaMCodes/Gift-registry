8d19be757cd9446a2590d3c870a79e6623527a33
MMempin421 <maria.mempin@hotmail.com>
2021-02-26 16:30:37 +1100

    Ami Paris

--- Scripts/app.R
+++ Scripts/app.R
@@ -20,10 +20,22 @@ ui <- fluidPage(
     fluidRow(column(h5(p(strong("AMI PARIS"), style = "text-align:center;color:black")), width = 12, style = "background-color:#cbeaee;border-radius:8px")),
     br(),
     fluidRow(
-      column(width = 4, img(src = "Ami_black.jpg", height="140", width="140"), align = "center", style = "background-color:white;padding:18px;border-radius:2px"), 
+      column(width = 4, img(src = "Ami_black.jpg", height="140", width="130"), align = "center", style = "background-color:white;padding:18px;border-radius:2px"), 
     column(width = 8, p(strong("Description:"), "Ami de Coeur patch jumper", br(), strong("Colour:"), "Black", br(), strong("Size:"), "XS", br(), strong("Price:"), "$389", br(), 
              tags$a(href = "https://www.farfetch.com/au/shopping/women/ami-paris-ami-de-coeur-jumper-item-16051786.aspx?size=19", strong("Link to this item")), 
-             target = "blank", style = "color:black"), style = "background-color:#f8c8c8;padding:33px;border-radius:2px")),
+             target = "blank", br(), br(), em("Unidays discount available"), style = "color:black"), style = "background-color:#f8c8c8;padding:13px;border-radius:2px")),
+    br(),
+    fluidRow(
+      column(width = 4, img(src = "Ami_navy.jpg", height="140", width="130"), align = "center", style = "background-color:white;padding:18px;border-radius:2px"), 
+      column(width = 8, p(strong("Description:"), "Ami de Coeur patch jumper", br(), strong("Colour:"), "Navy", br(), strong("Size:"), "XS", br(), strong("Price:"), "$396", br(), 
+                          tags$a(href = "https://www.farfetch.com/au/shopping/women/ami-paris-ami-de-coeur-patch-jumper-item-15134774.aspx?size=22", strong("Link to this item")), 
+                          target = "blank", br(), br(), em("Unidays discount available"), style = "color:black"), style = "background-color:#f8c8c8;padding:13px;border-radius:2px")),
+    br(),
+    fluidRow(
+      column(width = 4, img(src = "Ami_brown.jpg", height="140", width="130"), align = "center", style = "background-color:white;padding:18px;border-radius:2px"), 
+      column(width = 8, p(strong("Description:"), "Ami de Coeur jumper", br(), strong("Colour:"), "Cognac Brown", br(), strong("Size:"), "XS", br(), strong("Price:"), "$309", br(), 
+                          tags$a(href = "https://www.farfetch.com/au/shopping/women/ami-paris-ami-de-coeur-jumper-item-16051859.aspx?size=19", strong("Link to this item")), 
+                          target = "blank", br(), br(), em("Unidays discount available"), style = "color:black"), style = "background-color:#f8c8c8;padding:13px;border-radius:2px")),
   )
 )
 
