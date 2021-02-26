08685508e1a422594d8fd34a8ec326c4f12e0bc4
MMempin421 <maria.mempin@hotmail.com>
2021-02-26 14:27:24 +1100

    Getting columns in line with one another

--- Scripts/app.R
+++ Scripts/app.R
@@ -2,6 +2,9 @@
 library(shiny)
 install.packages("shinydashboard")
 library(shinydashboard)
+install.packages("bootstrap")
+library(bootstrap)
+
 
 ui <- fluidPage(
   fluidRow(
@@ -9,15 +12,15 @@ ui <- fluidPage(
     HTML('<center><img src="Header.png" height="220", width="400"></center>'),  # used HTML to align to centre
     #img(src = "Header.png", align = "center", height = 200, width = 400) if not uing HTML but couldn't get it centred
     #dashboardBody(fluidRow(box(title = "Ami Paris", width = 12, background = "maroon", solidHeader = TRUE, "Ami de Coeur patch jumper")))
-    fluidRow(column(h5(p(strong("AMI PARIS"), style = "text-align:left;color:black")), width = 12, style = "background-color:lavender;border-radius:10px")),
-    fluidRow(column(img(src = "Header.png", height="150", width="220"), width = 4, align = "center", style = "background-color:lavender;padding:18px;border-radius:10px")), 
-    column(p("Ami de Coeur patch jumper", br(), "Colour: Black", br(), "Size: XS", br(), "Price: $389", br(), 
+    fluidRow(column(h5(p("AMI PARIS", style = "text-align:center;color:black")), width = 12, style = "background-color:lavender;border-radius:8px")),
+    br(),
+    fluidRow(
+      column(width = 4, img(src = "Header.png", height="150", width="220"), align = "center", style = "background-color:lavender;padding:18px;border-radius:2px"), 
+    column(width = 8, p("Ami de Coeur patch jumper", br(), "Colour: Black", br(), "Size: XS", br(), "Price: $389", br(), 
              tags$a(href = "https://www.farfetch.com/au/shopping/women/ami-paris-ami-de-coeur-jumper-item-16051786.aspx?size=19", "Link to this item"), 
-             target = "blank"), 
-           style = "color:black;background-color:lavender;padding:18px;border-radius:10px", width = 8),
+             target = "blank", style = "color:black"), style = "background-color:lavender;padding:38px;border-radius:2px")),
   )
 )
-
 
 
 
