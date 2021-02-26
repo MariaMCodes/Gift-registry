3be12cf407bbffdcb1d77fa31fd0d3f5e09b1a69
MMempin421 <maria.mempin@hotmail.com>
2021-02-26 15:34:46 +1100

    Updated image and resized layouts

--- Scripts/ui.R
+++ Scripts/ui.R
@@ -16,10 +16,10 @@ ui <- fluidPage(
     fluidRow(column(h5(p(strong("AMI PARIS"), style = "text-align:center;color:black")), width = 12, style = "background-color:#cbeaee;border-radius:8px")),
     br(),
     fluidRow(
-      column(width = 4, img(src = "Header.png", height="150", width="220"), align = "center", style = "background-color:white;padding:18px;border-radius:2px"), 
+      column(width = 4, img(src = "Ami_black.jpg", height="140", width="140"), align = "center", style = "background-color:white;padding:18px;border-radius:2px"), 
       column(width = 8, p(strong("Description:"), "Ami de Coeur patch jumper", br(), strong("Colour:"), "Black", br(), strong("Size:"), "XS", br(), strong("Price:"), "$389", br(), 
                           tags$a(href = "https://www.farfetch.com/au/shopping/women/ami-paris-ami-de-coeur-jumper-item-16051786.aspx?size=19", strong("Link to this item")), 
-                          target = "blank", style = "color:black"), style = "background-color:#f8c8c8;padding:38px;border-radius:2px")),
+                          target = "blank", style = "color:black"), style = "background-color:#f8c8c8;padding:33px;border-radius:2px")),
   )
 )
 
